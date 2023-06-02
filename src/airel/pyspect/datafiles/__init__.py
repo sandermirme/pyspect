bbcf601fa779e27a6b4155ff008912666f84a55e
Sander Mirme <sander.mirme@ut.ee>
2023-06-02 16:02:03 +0300

    Add comments file parsing

--- src/airel/pyspect/datafiles/__init__.py
+++ src/airel/pyspect/datafiles/__init__.py
@@ -1,1 +1,2 @@
 from .recordsfiles import RecordsFiles, read_records_date_range
+from .comments_files import read_comments, comments_sorted
---
+++ src/airel/pyspect/datafiles/comments_files.py
@@ -0,0 +1,23 @@
+from collections import namedtuple
+
+from .util import parse_spectops_time
+
+Comment = namedtuple('Comment', ['begin_time', 'end_time', 'opmode', 'text'])
+
+
+def read_comments(sourcefile):
+    res = []
+    for line in sourcefile:
+        if not line: continue
+        if line[0] == '#': continue
+        res.append(Comment(*(line.rstrip('\n\r').split('\t'))))
+
+    return comments_sorted(res)
+
+
+def comment_sort_key(comment):
+    return parse_spectops_time(comment[0])
+
+
+def comments_sorted(comments):
+    return sorted(comments, key=comment_sort_key)
